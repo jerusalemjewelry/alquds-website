f643b1e384735bc6dc66d9f6e5fb086cb1504c6d
jerusalemjewelry <jerusalemjewelry@yahoo.com>
2025-12-29 12:43:54 -0600

    Add files via upload

---
+++ pricing.js
@@ -0,0 +1,9 @@
+window.metalPrices = {
+    "spotPrice24kOunce": 4532,
+    "silverPriceOunce": 80.00,
+    "platinumPriceOunce": 2461.49,
+    "openingPriceGold": 4266.20,
+    "openingPriceSilver": 79.33,
+    "openingPricePlatinum": 2246.56,
+    "lastUpdated": "2025-12-29T12:20:00.000Z"
+};
