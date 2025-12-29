3bc4815540530837742b722df1dd125f541f3981
jerusalemjewelry <jerusalemjewelry@yahoo.com>
2025-12-29 13:48:14 -0600

    Delete pricing.js

--- pricing.js
+++
@@ -1,9 +0,0 @@
-window.metalPrices = {
-    "spotPrice24kOunce": 4330.94,
-    "silverPriceOunce": 71.59,
-    "platinumPriceOunce": 2095.00,
-    "openingPriceGold": 4266.20,
-    "openingPriceSilver": 70.00,
-    "openingPricePlatinum": 2000.00,
-    "lastUpdated": "2025-12-29T12:50:00.000Z"
-};
